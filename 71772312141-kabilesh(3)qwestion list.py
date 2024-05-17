2dc0e176491224bd24a088040da2e54d47e24c82
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-17 11:30:05 +0530

    Update 71772312141-kabilesh(3)qwestion list.py

--- 71772312141-kabilesh(3)qwestion list.py
+++ 71772312141-kabilesh(3)qwestion list.py
@@ -1,3 +1,5 @@
 lst = [0, 1]
 [lst.append(lst[k - 1] + lst[k - 2]) for k in range(2, 20)]
 print('First 20 Fibonacci numbers:', lst)
+output:
+First 20 Fibonacci numbers: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
