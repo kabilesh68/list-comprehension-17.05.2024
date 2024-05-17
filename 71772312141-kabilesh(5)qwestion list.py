540888f2131dc6e08302daa1ccc69ef74afb8c77
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-17 11:34:25 +0530

    Update 71772312141-kabilesh(5)qwestion list.py

--- 71772312141-kabilesh(5)qwestion list.py
+++ 71772312141-kabilesh(5)qwestion list.py
@@ -2,4 +2,7 @@ lst = [1, 2, 5, -11, -9, 10, 13, 15, -17, -19, -21, -23, 25, 27, -29]
 pos = [num for num in lst if num > 0]
 print(pos)
 neg = [num for num in lst if num < 0]
-print(neg)
+print(neg)
+output:
+[1,2,5,10,13,15,25,27]
+[-11,-9,-17,-19,-21,-23,-29]
