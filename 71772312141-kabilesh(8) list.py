96d39b3a712a80fa1bdafececeacac88fc567122
kabilesh68 <168735849+kabilesh68@users.noreply.github.com>
2024-05-17 11:37:54 +0530

    Update 71772312141-kabilesh(8) list.py

--- 71772312141-kabilesh(8) list.py
+++ 71772312141-kabilesh(8) list.py
@@ -3,3 +3,5 @@ rows, cols = (5, 4)
 arr = [[(4 * random.randint(10, 40)) for i in range(cols)] for j in
 range(rows)]
 print(arr)
+output:
+[[60, 136, 92, 160], [112, 160, 140, 112], [152, 64, 84, 152], [128, 92, 116, 60], [56, 148, 88, 116]]
